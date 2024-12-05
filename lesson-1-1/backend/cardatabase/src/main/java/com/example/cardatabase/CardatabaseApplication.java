d9e45040a367727cd2656e53c9bc958452d64766
Saeed Anabtawi <saeed.a.anabtawi@gmail.com>
2024-12-05 17:59:06 +0200

    int lesson 1-3 rename lesson 1 to lesson 1-1

---
+++ lesson-1-1/backend/cardatabase/src/main/java/com/example/cardatabase/CardatabaseApplication.java
@@ -0,0 +1,14 @@
+package com.example.cardatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardatabaseApplication {
+
+	public static void main(String[] args) {
+		// After adding this comment the application is restarted
+		SpringApplication.run(CardatabaseApplication.class, args);
+	}
+
+}
